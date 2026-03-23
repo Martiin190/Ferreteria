7374a616a2d3064610981da7e6772155fe883f8a
Martin <cucufatocarapato@gmail.com>
2026-03-23 02:52:46 -0700

    pila de cambios y tal

--- Ferreteria/Vistas/Login.Designer.cs
+++ Ferreteria/Vistas/Login.Designer.cs
@@ -28,23 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(486, 241);
+            this.textBox1.Location = new System.Drawing.Point(639, 221);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(159, 22);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(486, 287);
+            this.textBox2.Location = new System.Drawing.Point(639, 272);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(159, 22);
             this.textBox2.TabIndex = 1;
@@ -52,7 +55,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(401, 244);
+            this.label1.Location = new System.Drawing.Point(546, 227);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(59, 16);
             this.label1.TabIndex = 2;
@@ -61,7 +64,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(401, 290);
+            this.label2.Location = new System.Drawing.Point(546, 278);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(79, 16);
             this.label2.TabIndex = 3;
@@ -69,19 +72,29 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(486, 336);
+            this.button1.Location = new System.Drawing.Point(639, 323);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(159, 23);
             this.button1.TabIndex = 4;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(483, 478);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(1115, 584);
+            this.ClientSize = new System.Drawing.Size(910, 584);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -90,6 +103,7 @@
             this.Name = "Login";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,6 +116,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Ferreteria/bbdd/Conexion.cs
+++ Ferreteria/bbdd/Conexion.cs
@@ -1,12 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySqlConnector;
 
 namespace Ferreteria.bbdd
 {
     internal class Conexion
-    {
-    }
+
+        {
+            public static MySqlConnection conn;
+
+            private static readonly string url =
+                "Server=145.14.151.51; " +
+                "Database=u812167471_mascotas; " +
+                "User=u812167471_mascotas; " +
+                "port=3306; " +
+                "password=2026-Mascotas";
+
+            public static void conectar()
+            {
+                try
+                {
+                    conn = new MySqlConnection(url);
+                    conn.Open();
+                }
+                catch (MySqlException e)
+                {
+                    MessageBox.Show("Error al conectar con la base de datos.\n" + e.Message);
+                }
+
+            }
+
+            public static void cerrar()
+            {
+                if (conn != null)
+                {
+                    try
+                    {
+                        conn.Close();
+                    }
+                    catch (MySqlException e)
+                    {
+                        MessageBox.Show("Error al cerrar la conexión con la base de datos.\n" + e.Message);
+                    }
+                }
+
+            }
+
+            public static bool acceder(string user, string pass)
+            {
+                string consulta = "SELECT usuario FROM usuarios " +
+                    "WHERE usuario = ?user AND pass = ?pass";
+
+                conectar();
+
+                try
+                {
+                    MySqlCommand comando = new MySqlCommand(consulta, conn);
+
+                    comando.Parameters.AddWithValue("?user", user);
+                    comando.Parameters.AddWithValue("?pass", pass);
+
+                    MySqlDataReader lector = comando.ExecuteReader();
+
+                    if (lector.Read())
+                    {
+                        return true;
+                    }
+                }
+                catch (MySqlException e)
+                {
+                    MessageBox.Show("Error en la consulta a base de datos.\n" + e.Message);
+                }
+                finally
+                {
+                    cerrar();
+                }
+
+                return false;
+            }
+
+        }
 }
